df522bc64cc8b2495b8bd3b5a33e8b676bae8a3b
myinboxdurga <myinbox.durga@gmail.com>
2024-04-22 22:03:12 +0800

    script

---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MOVIES=("RRR" "House" "HomeAlone")
+
+echo "First movie is: ${MOVIES[0]}
+echo "Second movie is: ${MOVIES[1]}
+echo "All movies are: ${MOVIES[@]}
---
+++ variable.sh
@@ -0,0 +1,7 @@
+echo "Please enter username:"
+
+read -s USERNAME
+
+echo "Please enter password:"
+
+read -s PASSWORD
